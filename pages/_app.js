24771e2720e0c240841df62c3f838d41c908790e
Ívini <johnatanv@fariasbrito.com.br>
2021-02-01 16:58:23 -0300

    Adição da fotn web

--- pages/_app.js
+++ pages/_app.js
@@ -4,15 +4,25 @@ import { DefaultSeo } from 'next-seo'
 import db from './../db.json'
 
 const GlobalStyle = createGlobalStyle`
+
+  @font-face {
+                    font-family: 'Noto Sans TC';
+                    src: url('/fonts/NotoSansTC-Regular.otf');
+                    font-weight: bold;
+                    font-style: normal;
+                    font-display: swap;
+  }
+
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
     display:flex;
     flex-direction:column;
-    font-family:'Lato',sans-serif;
+    font-family:'Noto Sans TC',sans-serif;
     color:${({ theme }) => theme.colors.contrastText};
   }
+
   html,body {
     height: 100vh;
   }
@@ -29,12 +39,7 @@ export default function App ({ Component, pageProps }) {
     <>
       <Head>
         <title>Alura Quiz | Valorant </title>
-        <link rel='preconnect' href='https://fonts.gstatic.com' />
         <link rel='shortcut icon' href='/favicon.png' />
-        <link
-          href='https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&amp;display=swap'
-          rel='stylesheet'
-        />
       </Head>
       <DefaultSeo
         openGraph={{
@@ -55,6 +60,7 @@ export default function App ({ Component, pageProps }) {
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Component {...pageProps} />
+         
       </ThemeProvider>
     </>
   )
